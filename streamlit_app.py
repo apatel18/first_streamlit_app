c9d3de0dbf94f8484adb4aa243a96c0c71107c52
apatel18 <Anavil.12345@gmail.com>
2022-08-30 17:27:05 -0400

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title("My Parent's New Diner")
-st.header('Breakfast Menu')
+streanlit.header('Breakfast Menu')
 streamlit.subheader('Omega 3 & Blueberry Oatmeal')
 streanlit.subheader('Kale, Spinach & Rocket Smoothie')
 streanlit.subheader('Hard-Boiled Free-Range Egg')
