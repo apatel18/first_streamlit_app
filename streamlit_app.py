5479c213eb16a3489ce974f48d684d704af510a1
apatel18 <Anavil.12345@gmail.com>
2022-08-30 17:28:58 -0400

    Update streamlit_app.py
    changing subheader to caption

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title("My Parent's New Diner")
 streamlit.header('Breakfast Menu')
-streamlit.subheader('Omega 3 & Blueberry Oatmeal')
-streamlit.subheader('Kale, Spinach & Rocket Smoothie')
-streamlit.subheader('Hard-Boiled Free-Range Egg')
+streamlit.caption('Omega 3 & Blueberry Oatmeal')
+streamlit.caption('Kale, Spinach & Rocket Smoothie')
+streamlit.caption('Hard-Boiled Free-Range Egg')
 
