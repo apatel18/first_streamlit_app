1d6a9cff0a963c956301b8a82ec3adef45033aba
apatel18 <Anavil.12345@gmail.com>
2022-08-30 17:26:30 -0400

    Update streamlit_app.py
    Added header
    Added subheaders

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title("My Parent's New Diner")
+st.header('Breakfast Menu')
+streamlit.subheader('Omega 3 & Blueberry Oatmeal')
+streanlit.subheader('Kale, Spinach & Rocket Smoothie')
+streanlit.subheader('Hard-Boiled Free-Range Egg')
+
